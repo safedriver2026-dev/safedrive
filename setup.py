4bbfcc11d9c8b0fe13da43424e5e44404c9fa846
safedriver2026-dev <safedriver2026@gmail.com>
2026-03-25 23:12:56 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="safedrive",
-    version="0.1.0",
+    version="0.1",
     packages=find_packages(),
-    install_requires=[
-        "pandas",
-        "pyarrow",
-        "openpyxl",
-        "requests",
-        "xgboost",
-        "lightgbm",
-        "catboost",
-        "folium",
-        "scikit-learn",
-        "google-generativeai"
-    ],
 )
