acc229271771913cdf56bc8e92364b3534781623
safedriver2026-dev <safedriver2026@gmail.com>
2026-03-25 22:18:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,15 @@ setup(
     name="safedrive",
     version="0.1.0",
     packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "pyarrow",
+        "openpyxl",
+        "requests",
+        "xgboost",
+        "lightgbm",
+        "catboost",
+        "folium",
+        "scikit-learn"
+    ],
 )
